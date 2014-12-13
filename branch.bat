75b7d2baca08f56e1115367c0871599e9b887438
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-12-13 10:44:41 -0200

    add batch for common git tasks

---
+++ branch.bat
@@ -0,0 +1,20 @@
+cd ..\data-model
+git checkout master
+git checkout -b %1
+git push --set-upstream origin %1
+cd ..\data-dao
+git checkout master
+git checkout -b %1
+git push --set-upstream origin %1
+cd ..\data-rest
+git checkout master
+git checkout -b %1
+git push --set-upstream origin %1
+cd ..\data-db
+git checkout master
+git checkout -b %1
+git push --set-upstream origin %1
+cd ..\data
+git checkout master
+git checkout -b %1
+git push --set-upstream origin %1
