9b52daaf8f557a1c19ed25385c082c25128401d8
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-12-16 09:55:54 -0200

    changes in merge and checkout batch

--- merge.bat
+++ merge.bat
@@ -1,20 +1,10 @@
 cd ..\data-model
-git checkout master
 git merge %1
-git push 
 cd ..\data-dao
-git checkout master
 git merge %1
-git push 
 cd ..\data-rest
-git checkout master
 git merge %1
-git push 
 cd ..\data-db
-git checkout master
 git merge %1
-git push 
 cd ..\data
-git checkout master
-git merge %1
-git push 
+git merge %1 
