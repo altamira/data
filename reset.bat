71c264062aadfaafdd6859501bcc1382c23ab72a
Alessandro Holanda <alessandro.holanda@hotmail.com>
2015-01-28 08:32:01 -0200

    add reset batch

---
+++ reset.bat
@@ -0,0 +1,15 @@
+cd ..\data-model
+git fetch origin
+git reset --hard origin/master
+cd ..\data-dao
+git fetch origin
+git reset --hard origin/master
+cd ..\data-rest
+git fetch origin
+git reset --hard origin/master
+cd ..\data-db
+git fetch origin
+git reset --hard origin/master
+cd ..\data
+git fetch origin
+git reset --hard origin/master
